887111bb4be9dbb33c40596c03b918df8bb9e5bb
Duco <duco@winterwerp.nl>
2017-04-06 20:58:59 +0200

    third egg

--- main.go
+++ main.go
@@ -16,11 +16,23 @@ type greet struct {
 	Subtitle string `json:"subtitle"`
 }
 
+type sumRequest struct {
+	Start   int   `json:"start"`
+	End     int   `json:"end"`
+	Numbers []int `json:"numbers"`
+}
+
+type sumResponse struct {
+	Answer       int `json:"answer"`
+	Contributers int `json:"contributers"`
+}
+
 func main() {
 	s := server{name: "Golang hands-on"}
 
 	http.Handle("/", http.FileServer(http.Dir("./resources/")))
 	http.HandleFunc("/api/greet/", s.titleHandler)
+	http.HandleFunc("/api/sum/", s.sumHandler)
 
 	err := http.ListenAndServe(":8080", nil)
 	log.Fatal(err)
@@ -45,6 +57,28 @@ func (s server) titleHandler(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		log.Println(err)
 	}
+}
+
+func (s server) sumHandler(w http.ResponseWriter, r *http.Request) {
+	var sreq sumRequest
+	d := json.NewDecoder(r.Body)
+	err := d.Decode(&sreq)
+
+	if err != nil {
+		log.Println(err)
+	}
+
+	var answer int
+	contributors := sreq.End - sreq.Start
+
+	for _, element := range sreq.Numbers[sreq.Start:sreq.End] {
+		answer += element
+	}
+
+	w.Header().Set("Content-Type", "application/json; charset=utf8")
+
+	e := json.NewEncoder(w)
+	err = e.Encode(sumResponse{Answer: answer, Contributers: contributors})
 }
 
 func speak(name string) string {
