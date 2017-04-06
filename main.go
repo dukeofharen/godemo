8b52a8adebdffa869a5ab1761ffa6b465b5e5146
Duco <duco@winterwerp.nl>
2017-04-06 19:44:21 +0200

    Added web server functionality

--- main.go
+++ main.go
@@ -1,21 +1,44 @@
 package main
 
 import (
-	"fmt"
-	"os"
+	"log"
+	"net/http"
 	"text/template"
 )
 
+type server struct {
+	template *template.Template
+}
+
 func main() {
-	sentence := speak("Duco")
-	fmt.Printf("The parrot says %s\n", sentence)
+	t := template.Must(template.ParseFiles("templates/index.html"))
+	s := server{template: t}
 
-	t, e := template.ParseFiles("template.tpl")
-	if e != nil {
-		panic(e)
+	http.HandleFunc("/", s.handleRoot)
+	http.Handle("/resources/", http.FileServer(http.Dir(".")))
+
+	err := http.ListenAndServe(":8080", nil)
+	log.Fatal(err)
+
+	// sentence := speak("Duco")
+	// fmt.Printf("The parrot says %s\n", sentence)
+
+	// t, e := template.ParseFiles("template.tpl")
+	// if e != nil {
+	// 	panic(e)
+	// }
+
+	// t.Execute(os.Stdout, "Quintor")
+}
+
+func (s *server) handleRoot(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf8")
+
+	err := s.template.Execute(w, "Vrolijk pasen!")
+
+	if err != nil {
+		log.Println(err)
 	}
-
-	t.Execute(os.Stdout, "Quintor")
 }
 
 func speak(name string) string {
